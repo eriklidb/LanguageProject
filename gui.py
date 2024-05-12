ae6aa3b74210af3396f518b25fd8949f2c43c10b
Erik Lidbjörk <eriklidb@kth.se>
2024-05-12 23:04:49 +0200

    User can now update word in text by clicking on it.

--- gui.py
+++ gui.py
@@ -35,6 +35,7 @@ class Window(tk.Tk):
         for i in range(self.num_words_displayed):
             label_word = tk.Label(self, text = "", background=self.background) 
             label_word.grid(sticky="W", row=i+1, column=0)
+            label_word.bind("<Button-1>", self.handle_word_press)
             self.labels_word.append(label_word)
 
             label_prob = tk.Label(self, text = "", background=self.background) 
@@ -46,10 +47,14 @@ class Window(tk.Tk):
         self.text_input.bind("<KeyRelease>", self.handle_keystroke)
 
     def handle_keystroke(self, _event: tk.Event) -> None:
-        input_str = self.text_input.get(1.0, "end-1c")
-        self.update_displayed_words(input_str)
+        self.update_displayed_words()
 
-    def update_displayed_words(self, input_str: str) -> None:
+    def handle_word_press(self, event: tk.Event) -> None:
+        pressed_word = event.widget.cget("text")
+        self.correct_text(pressed_word)
+
+    def update_displayed_words(self) -> None:
+        input_str = self.text_input.get(1.0, "end-1c")
         words, probs = self.word_probabilities.most_likely_words(input_str, self.num_words_displayed)
         for i in range(self.num_words_displayed):
             text_word = ""
@@ -60,6 +65,22 @@ class Window(tk.Tk):
             self.labels_word[i].config(text = text_word)
             self.labels_prob[i].config(text = text_prob)
 
+    def correct_text(self, pressed_word: str) -> None:
+        pressed_word = pressed_word + " "
+        input_str = self.text_input.get(1.0, "end-1c")
+        if len(input_str) == 0:
+            self.text_input.insert(1.0, pressed_word)
+        elif input_str[-1].isspace():
+            self.text_input.insert("end-1c", pressed_word)
+        else:
+            last_space_index = input_str.rfind(" ")
+            last_newline_index = input_str.rfind("\n")
+            replace_index = max(last_space_index, last_newline_index)
+            new_input_str = input_str[:replace_index + 1] + pressed_word
+            self.text_input.delete(1.0, "end-1c")
+            self.text_input.insert(1.0, new_input_str)
+        self.update_displayed_words()
+            
 # Start the event loop.
 if __name__ == '__main__':
     probabilities = NGramProbabilities('model.txt')
